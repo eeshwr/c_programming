fed3c4843fa6de4e200efc762093ac6b3217a65a
ish <eeshwr@gmail.com>
2021-05-20 21:53:13 +0545

    added header file

--- printsum.h
+++ printsum.h
@@ -1,12 +1,12 @@
 /*
  * =====================================================================================
  *
- *       Filename:  hello.c
+ *       Filename:  printsum.h
  *
- *    Description: 
+ *    Description:  
  *
  *        Version:  1.0
- *        Created:  20/5/21 08:44:50 अपराह्न +0545
+ *        Created:  20/5/21 09:50:44 अपराह्न +0545
  *       Revision:  none
  *       Compiler:  gcc
  *
@@ -19,14 +19,6 @@
 // header files
 #include<stdio.h> // print(), scanf()
 #include<conio.h> // getch()
-
-int print_sum(); // function declaration
-
-int main()
-{
-   print_sum(); // function call
-}
-
 int print_sum() //function defination
 {
     int a, b, sum;
