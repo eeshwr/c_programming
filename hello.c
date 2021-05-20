78f39d0b4a0abb0c24e58e100d482390cddb50f6
ish <eeshwr@gmail.com>
2021-05-20 20:47:03 +0545

    add new file

---
+++ hello.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description: 
+ *
+ *        Version:  1.0
+ *        Created:  20/5/21 08:44:50 अपराह्न +0545
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+int main(){
+    printf("hello world");
+
+}
+
