d8a90e2f69051dd6b880751f0c773c40c56f0b35
naresh <nareshtamang20000@gmail.com>
2021-05-21 19:13:41 +0545

    mama esma right angled triangle nai dekhauna milena k bho?

--- ram.c
+++ ram.c
@@ -14,10 +14,37 @@
  *   Organization:  
  *
  * =====================================================================================
- */
-#include"printsum.h"
-int main()
+ 
+ If the three sides of a triangle are entered through the keyboard,
+write a program to check whether the triangle is isosceles,
+equilateral, scalene or right angled triangle.
+*/
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+void main()
 {
-    print_sum();
+	int a, b , c;
+	printf("\n enter side a: \t");
+	scanf("%d", &a);
+	printf("\n enter side b: \t");
+	scanf("%d", &b);
+	printf("\n enter side c: \t");
+	scanf("%d", &c);
+	if(a==b && b==c && c==a)
+		printf("\n the triangle is equillibrium");
+	else
+	{
+		if(a==b || b==c || c==a)
+			printf("\n the triangle is isosceles");
+		
+		else
+		{
+			if( pow(a,2)=pow(b,2)+pow(c,2) || pow(b,2)=pow(a,2)+pow(c,2) || pow(c,2)=pow(b,2)+pow(a,2))
+				printf("\n the triangle is right angled traingle");
+			else
+				printf("\n the triangle is scalene");
+		}
+	}
 }
 
