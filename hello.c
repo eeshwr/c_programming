2f3c55200465a3d251b5819c0390d9bdf8b5937c
naresh <nareshtamang20000@gmail.com>
2021-05-21 18:42:00 +0545

    new program

--- hello.c
+++ hello.c
@@ -16,26 +16,28 @@
  * =====================================================================================
  */
 
-// header files
-#include<stdio.h> // print(), scanf()
-#include<conio.h> // getch()
+/*If the three sides of a triangle are entered through the keyboard,
+write a program to check whether the triangle is valid or not. The
+triangle is valid if the sum of two sides is greater than the largest of
+the three sides.*/
 
-int print_sum(); // function declaration
-
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
 int main()
 {
-   print_sum(); // function call
-}
-
-int print_sum() //function defination
-{
-    int a, b, sum;
-    printf("\n enter a:");
+   	int a,b,c;
+	printf("\n enter a");
     scanf("%d", &a);
-    printf("\n enter b:");
+    printf("\n enter b");
     scanf("%d", &b);
-    sum=a+b;
-    printf("sum of the number is %d", sum);
-    getch();
+    printf("\n enter c");
+    scanf("%d", &c);
+    if((a>b && a>c && b+c>a) || (b>c && b>a && a+c>b) || (c>a && c>b && a+b>c))
+    	printf("\n the triangle is valid");
+    else
+    	printf("\n the triangle is not valid");
+    		
+    	
 }
 
